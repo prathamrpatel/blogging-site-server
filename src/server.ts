0e03c621e56fd9b04a710054e5da127ea65232fa
prathamrpatel <rprathampatel@gmail.com>
2022-11-03 16:18:55 -0400

    init10

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ const server = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         secure: PRODUCTION,
       },
     })
